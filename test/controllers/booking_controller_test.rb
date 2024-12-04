e761380fa32a6089dd62954240dc45ca25fd59ae
QuantumTopia <nikiforos.rotas@gmail.com>
2024-12-04 17:06:08 +0400

    Add basic tests

--- test/controllers/booking_controller_test.rb
+++ test/controllers/booking_controller_test.rb
@@ -1,18 +1,30 @@
 require "test_helper"
 
 class BookingControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get booking_index_url
+  setup do
+    sign_in users(:one)
+    @event1 = events(:one)
+  end
+
+  test "should get index when logged in" do
+    get bookings_url
     assert_response :success
   end
 
-  test "should get new" do
-    get booking_new_url
+  test "should get new when logged in" do
+    get new_booking_url @event1.id
     assert_response :success
   end
 
-  test "should get create" do
-    get booking_create_url
-    assert_response :success
+  test "should not get index when logged out" do
+    sign_out users(:one)
+    get bookings_url
+    assert_response :redirect
+  end
+
+  test "should not get new when logged out" do
+    sign_out users(:one)
+    get new_booking_url @event1.id
+    assert_response :redirect
   end
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:one)
+    @event1 = events(:one)
+  end
+
   test "should get index" do
-    get events_index_url
+    sign_out users(:one)
+    get events_url
     assert_response :success
+  end
+
+  test "should not get new when logged out" do
+    sign_out users(:one)
+    get new_event_url
+    assert_response :redirect
   end
 end
